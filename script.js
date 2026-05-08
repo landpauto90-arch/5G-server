aa4d3df329db5be5640deed784c569025830daf6
landpauto90-arch <landpauto90@gmail.com>
2026-05-07 20:29:43 -0400

    Add toggleCode function to show/hide server code

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleCode() {
+  const code = document.getElementById("serverCode");
+  
+  if (code.style.display === "none") {
+    code.style.display = "block";
+  } else {
+    code.style.display = "none";
+  }
+}
